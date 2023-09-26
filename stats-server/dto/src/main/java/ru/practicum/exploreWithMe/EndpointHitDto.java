c3b15207cb9b72f14c266bb4d5dbbbb55d659280
Николай баранович <nick.fakov@yandex.ru>
2023-09-26 12:56:20 +0300

    stat-server 1.0

--- stats-server/dto/src/main/java/ru/practicum/exploreWithMe/EndpointHitDto.java
+++ stats-server/dto/src/main/java/ru/practicum/exploreWithMe/EndpointHitDto.java
@@ -2,7 +2,6 @@ package ru.practicum.exploreWithMe;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
