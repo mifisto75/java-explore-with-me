f7c5328dc410b0c4c3434997b57ed5c0f0b95211
Николай баранович <nick.fakov@yandex.ru>
2023-09-26 12:10:10 +0300

    stat-server 0.4

---
+++ ewm-service/src/main/java/ru/practicum/exploreWithMe/ShareItEwm.java
@@ -0,0 +1,10 @@
+package ru.practicum.exploreWithMe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareItEwm {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItEwm.class, args);
+    }
+}
