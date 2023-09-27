af04429997010b9318e26d497fc7260c6539ea94
Николай баранович <nick.fakov@yandex.ru>
2023-09-27 15:36:31 +0300

    stat-server 1.2

--- stats-server/client/src/main/java/ru/practicum/exploreWithMe/StatsClient.java
+++ stats-server/client/src/main/java/ru/practicum/exploreWithMe/StatsClient.java
@@ -1,9 +1,12 @@
 package ru.practicum.exploreWithMe;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -11,8 +14,14 @@ import java.util.Map;
 @Service
 public class StatsClient extends BaseClient {
     @Autowired
-    public StatsClient(RestTemplate rest) {
-        super(rest);
+    public StatsClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+
     }
 
 
